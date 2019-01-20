8d8484efdb47e331726e998c4e8cfb312613678b
Catnip <catnip@catnip.fyi>
2019-01-20 17:08:02 +0100

    added empty string initialization since handler is the root element

--- app/src/main/java/servant/servantandroid/internal/api_mirror/ModuleHandler.java
+++ app/src/main/java/servant/servantandroid/internal/api_mirror/ModuleHandler.java
@@ -2,14 +2,24 @@ package servant.servantandroid.internal.api_mirror;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONArray;
 
 import servant.servantandroid.internal.ApiService;
+import servant.servantandroid.internal.ServantInstance;
 
 public class ModuleHandler extends ApiElement<Module> {
     static final String API_ENDPOINT = "modules";
 
-    public ModuleHandler(ApiService service) { super(service); update(); }
+    public ModuleHandler(ApiService service) {
+        super(service);
+
+        // we don't have that stuff since we are the root element
+        // i will prolly change this in the future tho
+        m_fullname = "";
+        m_name = "";
+        m_id = "";
+
+        update();
+    }
 
     @Override
     public void updateValues(JSONObject data) throws JSONException {
@@ -18,7 +28,7 @@ public class ModuleHandler extends ApiElement<Module> {
     }
 
     @Override
-    Module instanciateChild(JSONObject json) {
+    protected Module instanciateChild(JSONObject json) {
         return new Module(json, m_api);
     }
 }
